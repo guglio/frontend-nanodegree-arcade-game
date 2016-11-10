98201049e21a7f5223a904d82e68cbdb15be5072
Guglielmo Turco <guglielmo.turco@gmail.com>
2016-11-10 15:13:13 +0100

    Add Heart.png to load

--- js/engine.js
+++ js/engine.js
@@ -175,7 +175,8 @@ var Engine = (function(global) {
         'images/char-cat-girl.png',
         'images/char-horn-girl.png',
         'images/char-pink-girl.png',
-        'images/char-princess-girl.png'
+        'images/char-princess-girl.png',
+        'images/Heart.png'
     ]);
     Resources.onReady(init);
 
