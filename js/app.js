ff0387115860abf48f375835fef9319589aaec92
Guglielmo Turco <guglielmo.turco@gmail.com>
2016-11-12 00:10:55 +0100

    Add gems.render() to player.render()

--- js/app.js
+++ js/app.js
@@ -127,6 +127,7 @@ var Player = function(){
 Player.prototype.render = function(){
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   hearts.render(this.lives);
+  gems.render(this.gems);
 };
 
 Player.prototype.update = function(){
